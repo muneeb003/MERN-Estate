a454df7adf30f98a82b99a03c0b11f014cf988ac
muneeb003 <mmuneebkhan2003@gmail.com>
2024-08-13 14:49:40 +0500

    Connected to MongoDB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 app.listen(8800, () => {
   console.log("Server is running on port 8800.");
 });
