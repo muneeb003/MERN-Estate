30077b90515ffb1e5f2e65e4c5e37a1fe829f4a2
muneeb003 <mmuneebkhan2003@gmail.com>
2024-08-16 12:39:49 +0500

    added functionality to the signup page. handled the cors issue. used fetch to send the request

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
 import authRoute from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,5 +1,39 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+    console.log(formData);
+  };
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (err) {
+      setLoading(false);
+      setError(err.message);
+    }
+  };
   return (
     <div className="mx-auto p-3 max-w-lg">
       <h1 className="font-semibold text-3xl text-center my-7">Sign Up</h1>
@@ -9,21 +43,28 @@ function SignUp() {
           placeholder="username"
           id="username"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="Email"
           id="email"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="password"
           id="password"
           className="border p-3 rounded-lg"
+          onChange={handleChange}
         />
-        <button className="border bg-slate-700 p-3 rounded-lg hover:opacity-95 text-white uppercase disabled:opacity-80">
-          Sign Up
+        <button
+          onClick={handleClick}
+          disabled={loading}
+          className="border bg-slate-700 p-3 rounded-lg hover:opacity-95 text-white uppercase disabled:opacity-80"
+        >
+          {loading ? "loading" : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5">
@@ -32,6 +73,7 @@ function SignUp() {
           <span className="text-blue-900">Sign In</span>
         </Link>
       </div>
+      {error && <span>{error}</span>}
     </div>
   );
 }
